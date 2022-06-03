c2eafbf9a7f829d9db9bbf5b5714d46fb1d2a575
JEFERSON FERREIRA DA SILVA <jeferson.ferreira.ca@gmail.com>
2022-06-03 17:37:07 -0300

    Implementing content negotiation to return JSON or XML

--- src/main/java/br/com/silva/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/silva/algafood/api/controller/CozinhaController.java
@@ -4,16 +4,18 @@ import br.com.silva.algafood.domain.model.Cozinha;
 import br.com.silva.algafood.domain.repository.CozinhaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas", produces =  MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController {
 
     @Autowired
