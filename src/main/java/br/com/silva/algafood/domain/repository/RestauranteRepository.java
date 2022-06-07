f3d285ea5b204ddda0f6a9d814d7bb12422b63e1
Jeferson Ferreira da Silva <jeferson.ferreira.ca@gmail.com>
2022-06-06 22:40:14 -0300

    Creating a Specifications Factory for Restaurantes

--- src/main/java/br/com/silva/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/br/com/silva/algafood/domain/repository/RestauranteRepository.java
@@ -1,9 +1,10 @@
 package br.com.silva.algafood.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import br.com.silva.algafood.domain.model.Restaurante;
 
-public interface RestauranteRepository  extends RestauranteRepositoryQueries, JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository  extends RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante>, JpaRepository<Restaurante, Long> {
 
 }
